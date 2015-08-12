ab1e57bab9dada8ea593cb15c3665a0d265848ad
NinhNguyen <ninhnguyen@ninhnguyens-mbp.dsldevice.lan>
2015-08-13 06:10:03 +0700

    Initial Commit

---
+++ CustomSwitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomSwitch
+//
+//  Created by NinhNguyen on 8/13/15.
+//  Copyright (c) 2015 NinhNguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
